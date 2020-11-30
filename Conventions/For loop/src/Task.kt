a14cadd0f2bb31c33f029a41d05f2faf3ffde94d
Casper Feng <casp.feng@gmail.com>
2020-11-30 23:23:05 +0100

    complete for loop

--- Conventions/For loop/src/Task.kt
+++ Conventions/For loop/src/Task.kt
@@ -1,4 +1,21 @@
-class DateRange(val start: MyDate, val end: MyDate)
+class DateRange(val start: MyDate, val end: MyDate):Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> {
+        return object: Iterator<MyDate> {
+            var now = start
+
+            override fun hasNext(): Boolean {
+                return now <= end
+            }
+
+            override fun next(): MyDate {
+                val res = now
+                if (hasNext()) now = now.followingDate() else throw NoSuchElementException()
+                return res
+            }
+
+        }
+    }
+}
 
 fun iterateOverDateRange(firstDate: MyDate, secondDate: MyDate, handler: (MyDate) -> Unit) {
     for (date in firstDate..secondDate) {
